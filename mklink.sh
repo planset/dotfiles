28791449d42e6814d125ae591577ceedb6e6baf6
Daisuke Igarashi <planset@gmail.com>
2012-02-09 15:44:38 +0900

    add mklink

---
+++ mklink.sh
@@ -0,0 +1,7 @@
+
+ln -s $HOME/dotfiles/.screenrc $HOME/
+ln -s $HOME/dotfiles/.screenrc.base $HOME/
+ln -s $HOME/dotfiles/.screenrc.dev $HOME/
+ln -s $HOME/dotfiles/.screenrc.layout $HOME/
+ln -s $HOME/dotfiles/.screen.defaultlayout $HOME/
+

72f537694b94ea02a9f06ab446581450bdc82203
Daisuke Igarashi <planset@gmail.com>
2012-04-20 04:33:18 +0900

    update, add pythonstartup

--- mklink.sh
+++ mklink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .screenrc .screenrc.dev .screen.defaultlayout)
+DOT_FILES=( .zshrc .gitconfig .gitignore .screenrc .screenrc.dev .screen.defaultlayout .pythonstartup)
 
 for file in ${DOT_FILES[@]}
 do
